e4ed3094bfce65b78817cd13fc6e3ae18d2caac2
cooljasonmelton <cooljasonmelton>
2020-08-09 05:44:01 -0500

    got video working in div messing with css

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import '../App.css';
+import hawkstorm from './hawkstorm-global.mp4';
+
 
 const VideoContainer = () => {
   return (
     <div className="VideoContainer cfb">
-        test
+
+        <video className="da-video" 
+            width="320" 
+            height="240" 
+            preload 
+            controls>
+            
+            <source src={hawkstorm} type="video/mp4"/>
+
+            Your browser does not support the video tag.
+        </video>
 
     </div>
   );
