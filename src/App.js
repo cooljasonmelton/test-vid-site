45cb3288a5f672199c532cf72cac066bc698cd10
cooljasonmelton <cooljasonmelton>
2020-08-12 21:41:32 -0500

    messing with styles for videocontainer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+
+// styles
 import './App.css';
+import '../node_modules/98.css/style.css'
+
+// components
 import VideoContainer from './components/VideoContainer';
 
 const App = () => {
   return (
-    <div className="App cfb">
+    <div className="App cfb window">
       <VideoContainer/>
 
     </div>
--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import '../App.css';
-import '../../node_modules/98.css/style.css'
 
 
 const VideoContainer = () => {
   return (
-    <div className="VideoContainer cfb">
+    <div className="VideoContainer window" >
+        <div className="title-bar">
+          <div className="title-bar-text"> Operation Samurai Eagle </div>
+          <div className="title-bar-controls">
+            <button>C</button>
+            <button>M</button>
+            <button>J</button>
+          </div>
+        </div>
 
-      <iframe width="1440" 
-        height="524" 
-        src="https://www.youtube.com/embed/w7fLe0prRa0" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-        allowfullscreen>
-
-      </iframe>
+      <div className="cfb">
+        <iframe width="1440" 
+          height="524" 
+          src="https://www.youtube.com/embed/w7fLe0prRa0" 
+          frameborder="0" 
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+          allowfullscreen>
+        </iframe>
+      </div>
 
     </div>
   );
