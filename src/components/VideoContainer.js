6d14d7dd40ab950b78529681427205302737d4b0
cooljasonmelton <cooljasonmelton>
2020-08-12 21:21:20 -0500

    installed 98.css

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import '../App.css';
+import '../../node_modules/98.css/style.css'
 
 
 const VideoContainer = () => {
   return (
     <div className="VideoContainer cfb">
 
-      <iframe width="1440" height="524" src="https://www.youtube.com/embed/w7fLe0prRa0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="1440" 
+        height="524" 
+        src="https://www.youtube.com/embed/w7fLe0prRa0" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen>
+
+      </iframe>
+
     </div>
   );
 }
