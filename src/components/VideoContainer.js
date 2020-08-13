4d4f772fa24a188f52fbd6aa51733fa43092985f
cooljasonmelton <cooljasonmelton>
2020-08-12 21:09:13 -0500

    got rid of big file, set up with youtube source instead of file

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import '../App.css';
-import hawkstorm from './hawkstorm-global.mp4';
 
 
 const VideoContainer = () => {
   return (
     <div className="VideoContainer cfb">
-      <video className="da-video" 
-          width="320" 
-          height="240" 
-          preload 
-          controls>
-          
-          <source src={hawkstorm} type="video/mp4"/>
 
-          Your browser does not support the video tag.
-      </video>
+      <iframe width="1440" height="524" src="https://www.youtube.com/embed/w7fLe0prRa0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   );
 }
